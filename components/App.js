87e4d7d798aff0e8ded32c448ec1028486aed42a
jonthomas-devpipeline <102564782+jonthomas-devpipeline@users.noreply.github.com>
2022-12-20 18:35:24 -0700

    Update App.js

--- components/App.js
+++ components/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
     return (
       <div className="App">
         <div>
-          <Counter />
+          <h1>App.js Heading </h1>
         </div>
       </div>
     );
